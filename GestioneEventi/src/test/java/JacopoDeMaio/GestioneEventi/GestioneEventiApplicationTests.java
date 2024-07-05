76f248468afcbd586c23367a246867da6223039c
Jacopo De maio <jajodemaio@gmail.com>
2024-07-05 10:58:07 +0200

    Initial commit

---
+++ GestioneEventi/src/test/java/JacopoDeMaio/GestioneEventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package JacopoDeMaio.GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
